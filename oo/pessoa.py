e23c114493684d81c66ae051d51830fab1de23b6
tici-dev <ticianelocutora@gmail.com>
2022-04-05 23:48:55 -0300

    criada a classe pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,22 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome:None, idade=35):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f"Olá {id(self)}"
+
+if __name__ == '__main__':
+    bia = Pessoa(nome='bia')
+    tici = Pessoa(bia, nome= 'tici')
+    print(id(tici))
+    print(tici.cumprimentar())
+    print(tici.nome)
+    print(tici.idade)
+    for filho in tici.filhos:
+        print(filho.nome)
+    tici.sobrenome = 'moreira'
+    del tici.filhos
+    print(tici.__dict__)
+    print(tici.__dict__)
